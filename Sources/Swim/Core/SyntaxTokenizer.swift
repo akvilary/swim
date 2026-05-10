12f484c463f3b01767e23df77dafeb5de36eab18
akvilary <akvilary@gmail.com>
2026-05-11 01:56:56 +0800

    Add built-in C++ syntax tokenizer

--- Sources/Swim/Core/SyntaxTokenizer.swift
+++ Sources/Swim/Core/SyntaxTokenizer.swift
@@ -57,6 +57,23 @@ struct SyntaxTokenizer {
         "switch", "type", "var",
     ]
 
+    static let cppKeywords: Set<String> = [
+        "alignas", "alignof", "and", "and_eq", "asm", "auto", "bitand", "bitor",
+        "bool", "break", "case", "catch", "char", "char8_t", "char16_t", "char32_t",
+        "class", "compl", "concept", "const", "consteval", "constexpr", "constinit",
+        "const_cast", "continue", "co_await", "co_return", "co_yield", "decltype",
+        "default", "delete", "do", "double", "dynamic_cast", "else", "enum",
+        "explicit", "export", "extern", "false", "float", "for", "friend", "goto",
+        "if", "inline", "int", "long", "mutable", "namespace", "new", "noexcept",
+        "not", "not_eq", "nullptr", "operator", "or", "or_eq", "private",
+        "protected", "public", "register", "reinterpret_cast", "requires", "return",
+        "short", "signed", "sizeof", "static", "static_assert", "static_cast",
+        "struct", "switch", "template", "this", "thread_local", "throw", "true",
+        "try", "typedef", "typeid", "typename", "union", "unsigned", "using",
+        "virtual", "void", "volatile", "wchar_t", "while", "xor", "xor_eq",
+        "override", "final",
+    ]
+
     static let jsKeywords: Set<String> = [
         "async", "await", "break", "case", "catch", "class", "const",
         "continue", "debugger", "default", "delete", "do", "else", "export",
@@ -70,6 +87,7 @@ struct SyntaxTokenizer {
         switch ext {
         case "swift": return swiftKeywords
         case "c", "h": return cKeywords
+        case "cpp", "cxx", "cc", "hpp", "hxx": return cppKeywords
         case "py": return pythonKeywords
         case "rs": return rustKeywords
         case "go": return goKeywords
