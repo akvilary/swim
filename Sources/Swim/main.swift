35b9e380c5bad34e7eba07227f8ce6284d031f75
akvilary <akvilary@gmail.com>
2026-05-16 19:44:58 +0800

    Add --version flag and release CI workflow

--- Sources/Swim/main.swift
+++ Sources/Swim/main.swift
@@ -1,5 +1,23 @@
 import Foundation
 
+let version = "0.0.1"
+
+if CommandLine.arguments.contains("--version") || CommandLine.arguments.contains("-v") {
+    print("swim \(version)")
+    exit(0)
+}
+
+if CommandLine.arguments.contains("--help") || CommandLine.arguments.contains("-h") {
+    print("swim - Vim-like terminal editor")
+    print("")
+    print("Usage: swim [file]")
+    print("")
+    print("Options:")
+    print("  -v, --version   Print version")
+    print("  -h, --help      Print help")
+    exit(0)
+}
+
 let filePath = CommandLine.arguments.count > 1 ? CommandLine.arguments[1] : nil
 let app = Application()
 app.run(filePath: filePath)
