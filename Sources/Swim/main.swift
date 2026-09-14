7baecd349bc9043e9402a5b596955a73f6789b13
akvilary <akvilary@gmail.com>
2026-09-14 15:49:58 +0800

    Bump version to 0.0.8

--- Sources/Swim/main.swift
+++ Sources/Swim/main.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-let version = "0.0.7"
+let version = "0.0.8"
 
 if CommandLine.arguments.contains("--version") || CommandLine.arguments.contains("-v") {
     print("swim \(version)")
